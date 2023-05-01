05340b4c9f534c9819c496a5ed35444e72bd16c3
Gao-Qiqiang <124568494+Gao-Qiqiang@users.noreply.github.com>
2023-05-01 14:44:07 +0100

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List
 import random
 
 grid1 = [
@@ -39,14 +39,18 @@ grid6 = [
 		[0, 0, 1, 0, 0, 0],
 		[0, 5, 0, 0, 6, 4]]
 
+# Main function: Solve Sudoku
 def solve_sudoku(board: List[List[int]]) -> bool:
-    n = len(board)
-    m = int(n ** 0.5)
+    n = len(board)  # Size of the Sudoku
+    m = int(n ** 0.5)  # Size of each small square in the Sudoku
+    # Initialize all possible numbers
     possible = [[set(range(1, n + 1)) for _ in range(n)] for _ in range(n)]
 
+    # Update possible values for cell (i, j)
     def update_possible(i: int, j: int, k: int) -> bool:
         conflict = False
         possible[i][j] = set([k])
+        # Update other cells in the same row and column
         for x in range(n):
             if k in possible[i][x]:
                 possible[i][x].remove(k)
@@ -56,6 +60,7 @@ def solve_sudoku(board: List[List[int]]) -> bool:
                 possible[x][j].remove(k)
                 if len(possible[x][j]) == 0:
                     conflict = True
+        # Update other cells in the same small square
         si, sj = m * (i // m), m * (j // m)
         for x in range(si, si + m):
             for y in range(sj, sj + m):
@@ -65,9 +70,11 @@ def solve_sudoku(board: List[List[int]]) -> bool:
                         conflict = True
         return conflict
 
+    # Wavefront propagation algorithm
     def wavefront_propagation() -> bool:
         while True:
             progress = False
+            # Traverse each cell, find determined values and update
             for i in range(n):
                 for j in range(n):
                     if board[i][j] == 0 and len(possible[i][j]) == 1:
@@ -76,12 +83,15 @@ def solve_sudoku(board: List[List[int]]) -> bool:
                         update_possible(i, j, k)
                         progress = True
 
+            # If there is no progress, break the loop
             if not progress:
                 break
 
+        # If all cells have values, return True
         if all(board[i][j] != 0 for i in range(n) for j in range(n)):
             return True
 
+        # Find the cell with the fewest optional values
         min_possible = float('inf')
         next_cell = None
         for i in range(n):
@@ -92,9 +102,11 @@ def solve_sudoku(board: List[List[int]]) -> bool:
                         min_possible = num_possible
                         next_cell = (i, j)
 
+        # If not found, return False
         if next_cell is None:
             return False
 
+        # Traverse all possible values and try
         i, j = next_cell
         for k in random.sample(possible[i][j], len(possible[i][j])):
             if not update_possible(i, j, k):
@@ -106,6 +118,7 @@ def solve_sudoku(board: List[List[int]]) -> bool:
 
         return False
 
+    # Update possible values based on initial board state
     for i in range(n):
         for j in range(n):
             if board[i][j] != 0:
@@ -113,7 +126,7 @@ def solve_sudoku(board: List[List[int]]) -> bool:
 
     return wavefront_propagation()
 
-if solve_sudoku(grid1):
+if solve_sudoku(grid1): # This function can enter the variable name of the sudoku
     print("Solve successfully：")
     for row in grid1:
         print(row)
